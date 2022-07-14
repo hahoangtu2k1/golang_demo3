3ca6c1d9c6453b3f8e261477e7b12c02ff54c5fa
hahoangtu2k1 <76681126+hahoangtu2k1@users.noreply.github.com>
2022-07-14 08:56:08 +0700

    add new

--- bai2.go
+++ bai2.go
@@ -5,12 +5,15 @@ import (
 )
 
 func mapMutex() {
+	numFor := 3
 	var X = make(map[string]string)
 	X["name"] = "Hà Hoàng Tú"
-	for i := 0; i <= 1000; i++ {
+	for j := 0; j <= numFor; j++ {
+		for i := 0; i <= 1000; i++ {
 
-		fmt.Println(X)
+			fmt.Println(X)
+		}
 	}
-	wg.Done()
+	// wg.Done()
 
 }
